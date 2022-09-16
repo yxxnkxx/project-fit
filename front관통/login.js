9266042cdad4dfc8a3d3b2650e7c666e0dce7d4d
Lee YunGyung <yxxnkxx@gmail.com>
2022-09-16 09:50:51 +0900

    front

--- front관통/login.js
+++ front관통/login.js
@@ -5,7 +5,7 @@ const userlist = JSON.parse(localStorage.getItem("users"));
 
 window.addEventListener("load", () => {
   const user = localStorage.getItem("user");
-  if (user!=="guest") {
+  if (user !== "guest") {
     alert("로그아웃 되었습니다.");
     localStorage.setItem("user", "guest");
     location.replace("index.html");
@@ -17,15 +17,18 @@ window.addEventListener("load", () => {
 
 
 const liBtn = document.querySelector("#loginBtn");
-liBtn.addEventListener("click", ()=>{
+liBtn.addEventListener("click", () => {
   console.log(idTag.value);
-  if(!idTag.value || !pwTag.value) {
+  if (!idTag.value || !pwTag.value) {
     alert("ID 와 비밀번호를 모두 입력하세요.");
   } else {
-    let user = userlist.find((obj)=>{
+
+
+    if (userlist !== null) { 
+    let user = userlist.find((obj) => {
       return obj.id === idTag.value && obj.password === pwTag.value;
     });
-    if(!user) {
+    if (!user) {
       alert("올바른 ID와 비밀번호를 입력하세요.")
     } else {
       alert("로그인 되었습니다.");
@@ -33,11 +36,14 @@ liBtn.addEventListener("click", ()=>{
       location.replace("index.html");
       window.open("index.html")
     }
+  } else {
+    alert("올바른 ID와 비밀번호를 입력하세요.")
+  }
   }
 });
 
 const reBtn = document.querySelector("#registBtn");
-reBtn.addEventListener("click", ()=>{
+reBtn.addEventListener("click", () => {
   location.replace("regist.html");
   window.open("regist.html");
 });
